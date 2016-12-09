427364bedc90d948e25a4e0c2318c92bb772d34d
Joe C <joeknows718@Joes-MacBook-Pro.local>
2016-12-08 20:11:07 -0500

    cbv

--- tutorial_project/tutorial_app/forms.py
+++ tutorial_project/tutorial_app/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from models import Page, Category, UserProfile
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
+from django.utils.crypto import get_random_string
 
 class UserForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput())
@@ -78,8 +79,39 @@ class ContactForm(forms.Form):
 
 		
 
+class PasswordRecoveryForm(forms.Form):
+	email = forms.EmailField(required=False)
 
+	def clean_email(self):
+		try:
+			return User.objects.get(email=self.cleaned_data['email'])
+		except User.DoesNotExist:
+			raise forms.ValidationError("Can't find a user based on this email")
+		return self.cleaned_data['email']
 
+	def reset_password(self):
+		user = self.clean_email()
+
+		password = get_random_string(length=8)
+
+		user.set_password(password)
+
+		user.save() 
+
+		body = """
+				Sorry you are having issues with your account! Below is your user name and new password
+
+				Username: {username}
+				Password: {password}
+
+				You can log in here: http://localhost:8000/login/
+				You can change your password here: http://localhost:8000/settings/
+
+				""".format(username=user.username, password=password)
+
+		pw_msg = EmailMessage('Your new password', body, 'joeknows718@gmail.com', [user.email])
+
+		pw_msg.send()
 
 
 
