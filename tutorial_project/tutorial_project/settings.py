7bd7798c7aecb0de9e81bcf6aa003120f288e09b
Joe C <joeknows718@Joes-MacBook-Pro.local>
2016-12-20 21:11:32 -0500

    live

--- tutorial_project/tutorial_project/settings.py
+++ tutorial_project/tutorial_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '1=rs=r8inq7&nnrlj(a#k&a(xir6e+wgnbj5hzk7#jd_jx8_c='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
